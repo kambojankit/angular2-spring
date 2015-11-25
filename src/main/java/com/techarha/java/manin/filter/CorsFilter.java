54ce5a009e5f650960f74092f68cd340ae6216ee
Ankit Kamboj <er.ankitkamboj@gmail.com>
2015-11-25 18:39:48 +0530

    CORS Filter

---
+++ src/main/java/com/techarha/java/manin/filter/CorsFilter.java
@@ -0,0 +1,35 @@
+package com.techarha.java.manin.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+/**
+ * Created by ankit on 25/11/15.
+ */
+@Component
+public class CorsFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        if (request.getHeader("Access-Control-Request-Method") != null
+                && "OPTIONS".equals(request.getMethod())) {
+            // CORS "pre-flight" request
+            response.addHeader("Access-Control-Allow-Methods",
+                    "GET, POST, PUT, DELETE");
+            response.addHeader("Access-Control-Allow-Headers",
+                    "X-Requested-With,Origin,Content-Type, Accept");
+        }
+        filterChain.doFilter(request, response);
+    }
+
+}
